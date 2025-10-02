cf8e6ff20217e9a740d631d20287cc6a36d48364
Eugene Naumov <rpbl3Luk@gmail.com>
2025-10-02 23:54:23 +0500

    My first

---
+++ configuration.py
@@ -0,0 +1,5 @@
+URL_SERVICE = "https://a5f6d380-cd14-4210-8bf7-17856209439e.serverhub.praktikum-services.ru/"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/" 
